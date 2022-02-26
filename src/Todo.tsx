824a1d1e645d5c38984a67b388736fe5e52a725a
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-26 18:10:39 +0900

    型定義の効率化

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,12 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+import { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+// Pickはこのコンポーネントに必要な型指定を抜き取るイメージで使う
+// props: Pick<TodoType, "userId" | "title" | "completed">
+
+//　　OmitはPickとは逆で必要ではない型指定を抜き取る書き方もある(こちらの方がコンパクトにコードが書ける)
+// props: Omit<TodoType, "id"
+
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
 
